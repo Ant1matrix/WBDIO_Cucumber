c10f95cae07cd627aa1928b35c64df39839ca74f
Anton Aleksandrov <anton.aleksandrov@icloud.com>
2024-04-02 17:41:03 +0300

    Added login_form feature file and steps draft

---
+++ features/step-definitions/ideal.login_form.steps.js
@@ -0,0 +1,18 @@
+import { Given } from "@wdio/cucumber-framework";
+
+Given('the user is on login form page', async () =>{
+
+});
+
+When(/^user inputs their username and password$/, () => {
+	
+});
+
+When('clicks on submit button', () => {
+  // Write code here that turns the phrase above into concrete actions
+});
+
+Then('a warning message pops up', () => {
+    // Write code here that turns the phrase above into concrete actions
+});
+  
